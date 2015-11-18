3762729dce23f408aaf8abb1fc0eb8e42c01a22a
charlesecollan <charles.ecollan@outlook.com>
2015-11-18 08:07:42 +0100

    Ajout du TP2 finalisé

---
+++ workspace/iuttp2/src/edu/iut/app/AbstractApplicationLogTest.java
@@ -0,0 +1,14 @@
+package edu.iut.app;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AbstractApplicationLogTest {
+
+	@Test
+	public void getMessagetest() {
+		fail("Not yet implemented");
+	}
+
+}
